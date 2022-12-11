cc2d7bfcc24bad308d6c68cc329d6e181beeaa49
Tomasz Dopierała <tomasz@dopierala.net.pl>
2022-12-11 23:43:19 +0100

    Initial changes

---
+++ src/views/MonitorView.vue
@@ -0,0 +1,11 @@
+<template>
+	<div class="home">
+		<h1>{{ name }} hello there</h1>
+	</div>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const name = ref('MonitorView');
+</script>
